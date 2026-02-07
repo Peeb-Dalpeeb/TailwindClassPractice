f004d6f846826bdc064329a3168fa6c59fabe37a
Scott Dalpias <instructionalmotion@gmail.com>
2026-02-06 21:08:57 -0600

    Finished form Styling

--- src/Components/Form.tsx
+++ src/Components/Form.tsx
@@ -1,5 +1,5 @@
 import { TextField } from './TextField';
-import { TextFieldComment } from './TextFieldComment';
+import { TextAreaField } from './TextAreaField';
 
 interface FormProps {
   title: string;
@@ -8,21 +8,21 @@ interface FormProps {
 
 export function Form({ title, description }: FormProps) {
   return (
-    <section className="mx-auto flex w-2/3 flex-col">
-      <h2 className="text-2xl font-bold">{title}</h2>
+    <section className="mx-auto flex w-full max-w-2xl flex-col px-4">
+      <h2 className="text-2xl font-extrabold">{title}</h2>
       <p className="mt-2">{description}</p>
       <form className="mt-8 flex flex-col gap-4">
         <TextField label="Full Name" />
         <TextField label="Email" type="email" />
         <TextField label="Phone" />
-        <TextFieldComment label="Comment" />
+        <TextAreaField label="Comment" />
+        <button
+          type="submit"
+          className="bg-pie-orange/30 mt-4 mr-auto px-12 py-2 text-xs tracking-wide uppercase cursor-pointer mb-10"
+        >
+          Submit
+        </button>
       </form>
-      <button
-        type="submit"
-        className="bg-pie-orange/30 mt-4 mr-auto px-10 py-2 text-xs tracking-wide uppercase"
-      >
-        Submit
-      </button>
     </section>
   );
 }
